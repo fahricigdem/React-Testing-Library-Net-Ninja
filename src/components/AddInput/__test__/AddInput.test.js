a5d1e8610cab976c4c73b993787c7fe72abd9f17
FAHRI CIGDEM <64100513+fahricigdem@users.noreply.github.com>
2021-10-07 13:10:09 +0200

    Input Component Unit tests

---
+++ src/components/AddInput/__test__/AddInput.test.js
@@ -0,0 +1,46 @@
+import { render, screen, fireEvent } from '@testing-library/react'
+import AddInput from "../AddInput"
+
+
+const mockedSetTodo = jest.fn()
+
+describe("AddInput", () => {
+    it('AddInput test-1', async () => {
+        render(
+            <AddInput
+                todos={[]}
+                setTodos={mockedSetTodo}
+            />
+        )
+        const element = screen.getByPlaceholderText(/Add a new task here.../i)
+        expect(element).toBeInTheDocument()
+    })
+
+
+    it('AddInput test-2', async () => {
+        render(
+            <AddInput
+                todos={[]}
+                setTodos={mockedSetTodo}
+            />
+        )
+        const element = screen.getByPlaceholderText(/Add a new task here.../i)
+        fireEvent.change(element, { target: { value: "Go out!" } })
+        expect(element.value).toBe("Go out!")
+    })
+
+    it('AddInput test-3', async () => {
+        render(
+            <AddInput
+                todos={[]}
+                setTodos={mockedSetTodo}
+            />
+        )
+        const element = screen.getByPlaceholderText(/Add a new task here.../i)
+        const buttonElement = screen.getByRole("button")
+        fireEvent.change(element, { target: { value: "Go out!" } })
+        fireEvent.click(buttonElement)
+        expect(element.value).toBe("")
+    })
+
+})
--- src/components/TodoFooter/__test__/TodoFooter.test.js
+++ src/components/TodoFooter/__test__/TodoFooter.test.js
@@ -11,39 +11,62 @@ const MockTodoFooter = ({ number }) => {
     )
 }
 
-it('render correct number of incompleted tasks ', async () => {
-    render(<MockTodoFooter number={1} />)
-    const element = screen.getByText(/1 task left/i)
-    expect(element).toBeInTheDocument()
+describe("TodoFooter", () => {
+    it('TodoFooter test-1a', async () => {
+        render(<MockTodoFooter number={1} />)
+        const element = screen.getByText(/1 task left/i)
+        expect(element).toBeInTheDocument()
+    })
+
+    it('TodoFooter test-1b', async () => {
+        render(<MockTodoFooter number={5} />)
+        const element = screen.getByText(/5 tasks left/i)
+        expect(element).toBeInTheDocument()
+    })
 })
 
 
-it('render correct number of incompleted tasks ', async () => {
+
+it('TodoFooter test-2', async () => {
     render(<MockTodoFooter number={1} />)
     const element = screen.getByText(/1 task left/i)
     expect(element).toBeTruthy()
 })
 
-it('is it Visible ', async () => {
-    render(<MockTodoFooter number={1} />)
-    const element = screen.getByText(/1 task left/i)
-    expect(element).toBeVisible()
-})
-
-// it('is it Visible ', async () => {
+// it('TodoFooter test-3', async () => {
 //     render(<MockTodoFooter number={1} />)
 //     const element = screen.getByText(/1 task left/i)
-//     expect(element).not.toBeVisible()
+//     expect(element).toBeVisible()
 // })
 
-it('is it Visible ', async () => {
-    render(<MockTodoFooter number={1} />)
-    const element = screen.getByText(/1 task left/i)
-    expect(element).toContainHTML('p')
-})
+// // it('TodoFooter test-4', async () => {
+// //     render(<MockTodoFooter number={1} />)
+// //     const element = screen.getByText(/1 task left/i)
+// //     expect(element).not.toBeVisible()
+// // })
 
-it('has it text ', async () => {
-    render(<MockTodoFooter number={1} />)
-    const element = screen.getByTestId("para")
-    expect(element).toHaveTextContent("1 task left")
-})
+// it('TodoFooter test-5', async () => {
+//     render(<MockTodoFooter number={1} />)
+//     const element = screen.getByText(/1 task left/i)
+//     expect(element).toContainHTML('p')
+// })
+
+// it('TodoFooter test-6', async () => {
+//     render(<MockTodoFooter number={1} />)
+//     const element = screen.getByTestId("para")
+//     expect(element).not.toBeFalsy()
+// })
+
+// it('TodoFooter test-7', async () => {
+//     render(<MockTodoFooter number={3} />)
+//     const element = screen.getByTestId("para")
+//     expect(element.innerHTML).toBe("3 tasks left")
+// })
+
+// it('TodoFooter test-', async () => {
+//     render(<MockTodoFooter number={3} />)
+//     const element = screen.getByTestId("para")
+//     expect(element.textContent).toBe("3 tasks left")
+//     expect(element).not.toBeFalsy()
+//     expect(element).toContainHTML('p')
+// })
